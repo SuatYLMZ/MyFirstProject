4feaa542eccb1fada328a70ed3c831519f26d38a
Suat YILMAZ <furkansuatylmz@gmail.com>
2023-06-05 09:50:34 -0700

    First2

--- src/main/java/com/project/schoolmanagment/controller/ContactMessageController.java
+++ src/main/java/com/project/schoolmanagment/controller/ContactMessageController.java
@@ -1,12 +1,17 @@
 package com.project.schoolmanagment.controller;
 
 
+import com.project.schoolmanagment.payload.request.ContactMessageRequest;
 import com.project.schoolmanagment.payload.response.ContactMessageResponse;
 import com.project.schoolmanagment.payload.response.ResponseMessage;
 import com.project.schoolmanagment.service.ContactMessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("contactMessages")
@@ -15,8 +20,9 @@ public class ContactMessageController {
 
     private final ContactMessageService contactMessageService;
 
-    public ResponseMessage<ContactMessageResponse> save(){
-        return null;
+    @PostMapping("/save")
+    public ResponseMessage<ContactMessageResponse> save(@RequestBody @Valid ContactMessageRequest contactMessageRequest){
+        return contactMessageService.save(contactMessageRequest);
     }
 
 }
--- src/main/java/com/project/schoolmanagment/repository/ContactMessageRepository.java
+++ src/main/java/com/project/schoolmanagment/repository/ContactMessageRepository.java
@@ -1,4 +1,13 @@
 package com.project.schoolmanagment.repository;
 
-public interface ContactMessageRepository {
+import com.project.schoolmanagment.entity.concretes.ContactMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+
+public interface ContactMessageRepository extends JpaRepository<ContactMessage,Long> {
+
+    boolean existsByEmailEqualsAndDateEquals(String email, LocalDate date);
+
+
 }
