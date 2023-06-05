f7ce94132adbe19579ed99613f86f8992ba2410c
Suat YILMAZ <furkansuatylmz@gmail.com>
2023-06-05 08:13:31 -0700

    First1

--- src/main/java/com/project/schoolmanagment/entity/concretes/ContactMessage.java
+++ src/main/java/com/project/schoolmanagment/entity/concretes/ContactMessage.java
@@ -1,4 +1,27 @@
 package com.project.schoolmanagment.entity.concretes;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class ContactMessage {
+
+    //TODO check all generation types and strategies
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //better to give a naming contactMessageId, contactMessageName, contactMessageSubject
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String subject;
+
+    private String message;
+
+    private LocalDate date;
 }
