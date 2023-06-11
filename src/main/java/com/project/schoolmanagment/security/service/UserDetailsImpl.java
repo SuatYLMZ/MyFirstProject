67681e6377ea235a5505ea56bb3e5a2e1e3c75c5
Suat YILMAZ <furkansuatylmz@gmail.com>
2023-06-11 09:52:44 -0700

    security started

---
+++ src/main/java/com/project/schoolmanagment/security/service/UserDetailsImpl.java
@@ -0,0 +1,121 @@
+package com.project.schoolmanagment.security.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.base.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+
+    private long id;
+
+
+    private String username;
+
+
+    private String name;
+
+
+    private Boolean isAdvisor;
+
+    @JsonIgnore
+    private String password;
+
+
+    private Collection<? extends GrantedAuthority>authorities;
+    // ? extends GrantedAuthority ==> means anything that extends from GrantedAuthority will be shown with this way
+    //                                otherwise we need to create millions of codes.
+
+
+    private UserDetailsImpl(Long id, String username, String name, Boolean isAdvisor, String password, String role){
+        this.id = id;
+        this.username= username;
+        this.name = name;
+        this.isAdvisor= isAdvisor;
+        this.password= password;
+        List<GrantedAuthority>grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(role));
+        this.authorities= grantedAuthorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    public boolean equals(Object o){
+        if (this==o){
+            return true;
+        }
+        if (o==null||getClass()!=o.getClass()){
+            return false;
+        }
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equal(id,user.getId());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
