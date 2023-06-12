c0b020e620f8eab2648aebd230deded1ed0dab81
Suat YILMAZ <furkansuatylmz@gmail.com>
2023-06-12 16:32:49 -0700

    day7

---
+++ src/main/java/com/project/schoolmanagment/payload/response/AuthResponse.java
@@ -0,0 +1,26 @@
+package com.project.schoolmanagment.payload.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.message.StringFormattedMessage;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthResponse {
+
+    private String username;
+    private String ssn;
+    private String role;
+    private String token;
+    private String name;
+    private String isAdvisory;
+
+
+
+}
