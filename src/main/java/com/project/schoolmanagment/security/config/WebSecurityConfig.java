a4d1c3ad90f8ae8df71f56d05846347a288aba6c
SuatYLMZ <furkansuatylmz@gmail.com>
2023-07-06 09:07:17 -0700

    End of the project

--- src/main/java/com/project/schoolmanagment/security/config/WebSecurityConfig.java
+++ src/main/java/com/project/schoolmanagment/security/config/WebSecurityConfig.java
@@ -112,14 +112,15 @@ public class WebSecurityConfig {
 	}
 
 	private static final String AUTH_WHITE_LIST[] = {
+			"/v3/api-docs/**",
+			"swagger-ui.html",
+			"/swagger-ui/**",
 			"/",
-			"/index*",
-			"/static/**",
-			"/*.js",
-			"/*.json",
-			"/contactMessages/save",
+			"index.html",
+			"/images/**",
+			"/css/**",
+			"/js/**",
 			"/auth/login"
-
 	};
 
 
